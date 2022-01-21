14048358d2ae00d0ee8cd61bbace6725e6678bde
pingo <pingstrong@163.com>
2022-01-21 12:45:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -267,7 +267,7 @@ InstallDocker()
 		#
         #if [[ $CountryId == 1 ]]; then
             # step 1: 安装必要的一些系统工具
-            sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+            sudo yum install -y yum-utils device-mapper-persistent-data lvm2 erase podman buildah
             # Step 2: 添加软件源信息
             sudo yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
             # Step 3: 更新并安装Docker-CE 官方软件源默认启用了最新的软件
